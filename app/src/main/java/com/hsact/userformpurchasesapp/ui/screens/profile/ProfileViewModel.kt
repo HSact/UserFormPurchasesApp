b1b2b202aaabd0360a549aefe49b1395e4762df1
Alex <actorlefter@gmail.com>
2025-08-01 02:44:13 +0300

    feat: Populate profile screen with user data
    This commit updates the `ProfileViewModel` to fetch and display the user's name and surname from the `GetUserDataUseCase`.
    
    Key changes:
    - `ProfileViewModel.kt`:
        - Removed unused `GetPurchasesUseCase` dependency.
        - In the `init` block, `viewModelScope` is launched to collect user data from `getUserDataUseCase`.
        - The collected `userData` (name and surname) is used to update the `_uiState`.
        - `filterNotNull()` is used to ensure that only non-null user data updates the UI.

--- app/src/main/java/com/hsact/userformpurchasesapp/ui/screens/profile/ProfileViewModel.kt
+++ app/src/main/java/com/hsact/userformpurchasesapp/ui/screens/profile/ProfileViewModel.kt
@@ -2,19 +2,17 @@ package com.hsact.userformpurchasesapp.ui.screens.profile
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.hsact.domain.usecase.GetPurchasesUseCase
 import com.hsact.domain.usecase.userdata.GetUserDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    private val getUserDataUseCase: GetUserDataUseCase,
-    private val getPurchasesUseCase: GetPurchasesUseCase
-) : ViewModel(){
+    private val getUserDataUseCase: GetUserDataUseCase
+) : ViewModel() {
     private val _uiState = MutableStateFlow(ProfileUiState())
     val uiState = _uiState
     fun switchBiometric() {
@@ -22,14 +20,17 @@ class ProfileViewModel @Inject constructor(
             isBiometricEnabled = !_uiState.value.isBiometricEnabled
         )
     }
-//    init {
-//        viewModelScope.launch {
-//            val a = getPurchasesUseCase().first()
-//            println(a)
-//        }
-//        viewModelScope.launch {
-//            val a = getUserDataUseCase().first()
-//            println(a)
-//        }
-//    }
+
+    init {
+        viewModelScope.launch {
+            getUserDataUseCase()
+                .filterNotNull()
+                .collect { userData ->
+                    _uiState.value = _uiState.value.copy(
+                        name = userData.name,
+                        surname = userData.surname
+                    )
+                }
+        }
+    }
 }
